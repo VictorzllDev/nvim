d7427ca7ed330f96d64f619de5d6f352c4220daf
VictorzllDev <VictorzllDev@gmail.com>
2024-05-12 18:15:27 -0300

    first settings

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,26 @@
+local plugins = {
+  {
+    "mhartington/formatter.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.formatter"
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "prettier",
+        "typescript-language-server"
+      }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+}
+return plugins
