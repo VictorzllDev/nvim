5adb425b783bb93667b64fa500f1432eb1d3e181
VictorzllDev <VictorzllDev@gmail.com>
2024-12-13 12:46:49 -0300

    Removido keymap para Tab e S-Tab no cmp e adicionado "Accept Next Word" no Codeium com S-Tab
    - Removido o mapeamento de teclas `Tab` e `S-Tab` do plugin `cmp` para
    evitar conflitos com outros atalhos.
    - Configurado o mapeamento da tecla `S-Tab` para a funcionalidade
    "Accept Next Word" do Codeium, facilitando a aceitação de sugestões de
    forma eficiente.
    - Alterações realizadas para melhorar a usabilidade e evitar
    sobreposição de funções no Neovim.

--- lua/custom/configs/codeium.lua
+++ lua/custom/configs/codeium.lua
@@ -5,3 +5,4 @@ vim.keymap.set('i', '<M-]>', function() return vim.fn['codeium#CycleCompletions'
 vim.keymap.set('i', '<M-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
 vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
 vim.keymap.set('i', '<M-Bslash>', function() return vim.fn['codeium#Complete']() end, { expr = true, silent = true })
+vim.keymap.set('i', '<S-Tab>', function() return vim.fn['codeium#AcceptNextWord']() end, { expr = true, silent = true })
