3b2aad3c39e240dc20f3996fa5c90716c956fa3a
VictorzllDev <VictorzllDev@gmail.com>
2025-06-23 12:53:12 -0300

    add yaml tressiter, trocando theme, add codeaction
    - add yaml no treesitter
    - Trocando theme para dark horizon
    - add codeaction no atalho "ca"

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -14,6 +14,7 @@ return {
 		"typescript",
 		"vim",
 		"vimdoc",
+		"yaml",
 	},
 
 	highlight = {
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,6 @@
 require("nvchad.mappings")
 
 -- Atalhos gerais
--- local map = vim.keymap.set
+local map = vim.keymap.set
+
+map("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { silent = true, desc = "Code Action" })
